a081dce8a1389365efbfdac0c972bdde245ce9ce
xboston <bost56@gmail.com>
2013-08-09 22:46:46 +0600

    Заработало

--- test.php
+++ test.php
@@ -15,20 +15,41 @@ interface DrupalStreamWrapperInterface
 {
 }
 
-;
+
+$client = new MogileFs();
+$client->connect('46.254.22.222' , 7001 , 'test.mogile');
+//$paths = $client->fileInfo('/lkLK69KRpwE.jpg');
+$client->put('5555.jpg', '/girl2.jpg', 'class2');
+$paths = $client->get('/girl2.jpg');
+$client->close();
+
+print_r($paths);
+die;
+
+
+
+
 
 require './MediaIhcMogileStreamWrapper.inc';
 
-stream_wrapper_register('ihcmogilefs' , 'MediaIhcMogileStreamWrapper');
+stream_wrapper_register('ihcmogile' , 'MediaIhcMogileStreamWrapper');
 
-$fileKey = '/video/TWRFfa_T_b_NI.jpg1';
+$f = 'ihcmogile://lkLK69KRpwE.jpg';
+echo file_exists($f);
+
+die;
+
+$fileKey = '/video/TWRFfa_T_b_NI.jpg';
 
 $x = 'test5';
 
 //file_put_contents(sprintf('ihcmogilefs://%s' , $fileKey) , $x);
+file_get_contents(sprintf('ihcmogilefs://%s' , $fileKey) , $x);
+
+die;
 
 $client = new MogileFs();
-$client->connect('127.0.0.1' , 6001 , 'files.com');
+$client->connect('46.254.22.222' , 7001 , 'files.com');
 //$r = $client->put($x , $fileKey , 'files' , false);
 
 //filesize('ihcmogile://video/TWRFfa_T_b_NI.jpg');
